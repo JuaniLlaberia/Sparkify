c0aa7488397e2cdb0719da13197e51f6eaba03de
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-01-24 19:02:45 -0300

    Clean convex file

--- convex/auth.ts
+++ convex/auth.ts
@@ -2,8 +2,6 @@ import GitHub from '@auth/core/providers/github';
 import Google, { GoogleProfile } from '@auth/core/providers/google';
 import { convexAuth } from '@convex-dev/auth/server';
 
-import { MutationCtx } from './_generated/server';
-
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [
     GitHub({
