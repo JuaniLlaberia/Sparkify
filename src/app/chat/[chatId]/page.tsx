ef6136d631ae7dca34cc8faa3065760af69ada29
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-01-25 14:06:40 -0300

    Re-Implement chatId page with new approach

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -1,10 +1,21 @@
+'use client';
+
+import { Download, Rocket } from 'lucide-react';
+import { useState } from 'react';
+import { useParams } from 'next/navigation';
+
 import Header from '@/components/custom/header';
 import ChatView from './(components)/chat-view';
 import CodeView from './(components)/code-view';
 import { Button } from '@/components/ui/button';
-import { Download, Rocket } from 'lucide-react';
+import { Id } from '../../../../convex/_generated/dataModel';
 
 const ChatPage = () => {
+  const [isLoadingMessage, setIsLoadingMessage] = useState<boolean>(false);
+  const [isLoadingCode, setIsLoadingCode] = useState<boolean>(false);
+
+  const { chatId } = useParams<{ chatId: Id<'chats'> }>();
+
   return (
     <div className='flex flex-col w-full'>
       <Header
@@ -26,8 +37,16 @@ const ChatPage = () => {
         }
       />
       <div className='min-h-[calc(100vh-4rem)] p-5 grid grid-cols-1 md:grid-cols-7 md:gap-4 xl:gap-10'>
-        <ChatView />
-        <CodeView />
+        <ChatView
+          chatId={chatId}
+          isLoading={isLoadingMessage}
+          handleChatLoader={setIsLoadingMessage}
+          handleCodeLoader={setIsLoadingCode}
+        />
+        <CodeView
+          chatId={chatId}
+          isLoading={isLoadingCode}
+        />
       </div>
     </div>
   );
