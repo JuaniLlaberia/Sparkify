79c5e40a027003a8a8d6eff652cd3512c51539ad
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-01-27 14:23:47 -0300

    Update reusable ui components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,12 +14,12 @@ const buttonVariants = cva(
         special:
           'bg-violet-500 text-white border-violet-500 shadow hover:bg-violet-500/90',
         destructive:
-          'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
+          'bg-destructive/75 text-destructive-foreground shadow-sm hover:bg-destructive/90',
         outline:
           'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
         secondary:
           'bg-muted/75 shadow-sm hover:bg-accent hover:text-accent-foreground',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        ghost: 'hover:bg-accent/75 hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
